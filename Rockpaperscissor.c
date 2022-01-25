ca60f71bafb37c5aeb90a46c57fc4bae9d37f0fc
Divyanshu Anand <divyanshu.goku@gmail.com>
2022-01-25 20:23:44 +0530

    Initial commit

---
+++ Rockpaperscissor.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int rockPaperScissor(char you, char comp)
+{
+
+    //rp
+    //ps
+    //rs
+    if (you == comp)
+    {
+        return 0;
+    }
+
+
+
+
+    if (you == 's' && comp == 'r')
+    {
+        return -1;
+    }
+    else if (you == 'r' && comp == 's')
+    {
+        return 1;
+    }
+    if (you == 'p' && comp == 'r')
+    {
+        return -1;
+    }
+    else if (you == 'r' && comp == 'p')
+    {
+        return 1;
+    }
+    if (you == 'p' && comp == 's')
+    {
+        return -1;
+    }
+    else if (you == 's' && comp == 'p')
+    {
+        return 1;
+    }
+}
+    int main()
+    {
+        char you, comp;
+        srand(time(0));
+        int number = rand() % 100 + 1;
+        if (number < 33)
+        {
+            comp = 'r';
+        }
+        else if (number > 33 && number < 66)
+        {
+            comp = 'p';
+        }
+        else
+        {
+            comp = 's';
+        }
+        printf("Enter 'r' for rock,'p' for paper and 's' for scissor\n");
+        scanf("%c", &you);
+        int result = rockPaperScissor(you, comp);
+        if (result == 0)
+        {
+            printf("Game draw!\n");
+        }
+        else if (result == 1)
+        {
+            printf("You win\n");
+        }
+        else
+        {
+            printf("You lose\n");
+        }
+         printf("You chose %c and computer chose %c. ", you, comp);
+        return 0;
+    }
